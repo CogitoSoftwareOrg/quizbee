053e2d4e6beafe26dc28188e41db80b896b78f25
plw <kwazzartcreature@gmail.com>
2025-10-13 03:10:38 -0700

    parse coolify url for pr id

--- web/src/lib/pb.ts
+++ web/src/lib/pb.ts
@@ -1,4 +1,18 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.PB_URL);
-pb.autoCancellation(false);
+export function extractPrIdFromCoolifyUrl(coolify: string) {
+  const url = new URL(coolify);
+  const prId = url.hostname.split(".")[0];
+  return prId;
+}
+
+let url = import.meta.env.PB_URL;
+const coolify = import.meta.env.COOLIFY_URL;
+
+if (import.meta.env.PUBLIC_ENV === "preview" && coolify) {
+  const prId = extractPrIdFromCoolifyUrl(coolify);
+  url = url.replace("https://", `https://${prId}-`);
+}
+
+export const pb = new PocketBase(url);
+if (typeof window === "undefined") pb.autoCancellation(false);
