f70f95e702523c7dc456c385e0929aa0d064d159
plw <kwazzartcreature@gmail.com>
2025-10-17 11:39:46 -0700

    add runtime vars?

--- web/src/lib/pb.ts
+++ web/src/lib/pb.ts
@@ -2,8 +2,12 @@ import PocketBase from "pocketbase";
 
 import { urlWithPR } from "./pr-url";
 
-const adminUrl = urlWithPR(import.meta.env.PB_URL);
-const publicUrl = urlWithPR(import.meta.env.PUBLIC_PB_URL);
+const adminUrl = urlWithPR(
+  import.meta.env.PB_URL ?? process.env.RUNTIME_PB_URL
+);
+const publicUrl = urlWithPR(
+  import.meta.env.PUBLIC_PB_URL ?? process.env.RUNTIME_PUBLIC_PB_URL
+);
 
 export const pb = new PocketBase(adminUrl);
 pb.autoCancellation(false);
--- web/src/middleware/index.ts
+++ web/src/middleware/index.ts
@@ -7,7 +7,10 @@ export const onRequest = defineMiddleware(async (context, next) => {
   if (!pb.authStore.isValid) {
     await pb
       .collection("_superusers")
-      .authWithPassword(import.meta.env.PB_EMAIL, import.meta.env.PB_PASSWORD);
+      .authWithPassword(
+        import.meta.env.PB_EMAIL ?? process.env.RUNTIME_PB_EMAIL,
+        import.meta.env.PB_PASSWORD ?? process.env.RUNTIME_PB_PASSWORD
+      );
   }
 
   // Handle locale routing
