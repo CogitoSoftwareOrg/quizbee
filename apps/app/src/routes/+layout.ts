178f2bb0bfe2b7a22f59ea03a9687c160c0264e4
plw <kwazzartcreature@gmail.com>
2025-10-23 06:36:35 -0700

    correct posthog token (I am crazy)

--- apps/app/src/routes/+layout.ts
+++ apps/app/src/routes/+layout.ts
@@ -9,14 +9,7 @@ import { uiStore } from '$lib/apps/users/ui.svelte';
 export const load = async () => {
 	await uiStore.loadState();
 
-	const ENV = env.PUBLIC_ENV;
-	const POSTHOG_TOKEN =
-		ENV === 'production'
-			? env.PUBLIC_POSTHOG_PROD
-			: ENV === 'quality-assurance'
-				? env.PUBLIC_POSTHOG_QA
-				: env.PUBLIC_POSTHOG_LOCAL;
-
+	const POSTHOG_TOKEN = env.PUBLIC_POSTHOG;
 	if (POSTHOG_TOKEN)
 		posthog.init(POSTHOG_TOKEN, {
 			api_host: 'https://eu.i.posthog.com',
