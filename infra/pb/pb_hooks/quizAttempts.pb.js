805a113ad89dbb01efca45c81466f2945b841a6c
plw <kwazzartcreature@gmail.com>
2025-11-08 12:31:22 -0800

    hotfix: Update PB_VERSION to 0.32.0 in Dockerfiles and enhance quiz feedback handling in quizAttempts.pb.js

--- infra/pb/pb_hooks/quizAttempts.pb.js
+++ infra/pb/pb_hooks/quizAttempts.pb.js
@@ -1,12 +1,16 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onRecordUpdate((e) => {
+  const feedback = e.record.get("feedback");
   const choices = JSON.parse(e.record.get("choices") ?? "[]");
   const quizId = e.record.get("quiz");
 
   const quiz = $app.findRecordById("quizes", quizId);
+  const limit = quiz.getInt("itemsLimit");
 
-  if (!e.record.get("feedback") && quiz.get("itemsLimit") === choices.length) {
+  console.log(feedback, choices.length, limit);
+  if (feedback == "null" && limit === choices.length) {
+    console.log("feedback set {}");
     e.record.set("feedback", {});
   }
 
