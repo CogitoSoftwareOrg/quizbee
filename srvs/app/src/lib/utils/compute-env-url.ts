563fd69a6e5d970e8667fe71eaa0559a0584e033
plw <kwazzartcreature@gmail.com>
2025-11-29 05:42:08 -0800

    debugging preview linking

--- srvs/app/src/lib/utils/compute-env-url.ts
+++ srvs/app/src/lib/utils/compute-env-url.ts
@@ -1,11 +1,13 @@
 import { env } from '$env/dynamic/public';
 
 export function computeEnvUrl(url: string) {
+	console.log('env.PUBLIC_ENV', env.PUBLIC_ENV, url);
 	if (env.PUBLIC_ENV === 'preview') {
 		const base = new URL(url);
 		const prIdCandidate = window.location.hostname.split('-')[0];
 		if (/^\d+$/.test(prIdCandidate)) base.hostname = `${prIdCandidate}-${base.hostname}`;
 		return base.toString();
 	}
+	console.log('env.PUBLIC_ENV', env.PUBLIC_ENV, url);
 	return url;
 }
